4a41ff9acc3f0655684d6681b2387a6f80300ba0
QaAsos-Ishan <ishan.1789@gmail.com>
2019-08-13 18:26:41 +0100

    Added files

---
+++ SampleAPI/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using RestSharp;
+using RestSharp.Extensions;
+using RestSharp.Serialization.Json;
+using SampleAPI.Models;
+
+namespace SampleAPITest
+{
+    class Program
+    {
+        [Test]
+        public void TestGet()
+        {
+            var client = new RestClient("https://localhost:44395/");
+            var request = new RestRequest("api/item", Method.GET);
+            
+            var response = client.Execute(request);
+
+            var result = JsonConvert.DeserializeObject<List<Item>>(response.Content);
+
+            Assert.That(response.StatusCode,Is.EqualTo(HttpStatusCode.OK));
+            Assert.IsNotNull(result.First());
+        }
+    }
+}
---
+++ SampleAPI/SampleAPI/Controllers/ItemController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleAPI.Models;
+
+namespace SampleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public ItemController(Context context)
+        {
+            _context = context;
+
+            if (!_context.Items.Any())
+            {
+                // Create a new Item if collection is empty,
+                // which means you can't delete all Items.
+                _context.Items.Add(new Item { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetItem(int itemId)
+        {
+            var item = await _context.Items.FindAsync(itemId);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Item>> PostItem(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetItems), new { id = item.Id },item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItem(int id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var abc = await _context.Items.FindAsync(id);
+
+            if (abc == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(abc);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
